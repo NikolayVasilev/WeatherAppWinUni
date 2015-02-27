5675243cea3ed097f08a04b231cf89fe26257bd1
Nikolay Vasilev <n.vasilev.fmi@gmail.com>
2015-02-27 20:21:16 +0200

    Removed legacy code + icons

--- WeatherWizz/WeatherWizz/WeatherWizz.Windows/HubPage.xaml.cs
+++ WeatherWizz/WeatherWizz/WeatherWizz.Windows/HubPage.xaml.cs
@@ -12,7 +12,6 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using WeatherWizz.Data;
 using WeatherWizz.Common;
 using Windows.Graphics.Display;
 using WeatherWizz.DataModel;
--- WeatherWizz/WeatherWizz/WeatherWizz.WindowsPhone/HubPage.xaml.cs
+++ WeatherWizz/WeatherWizz/WeatherWizz.WindowsPhone/HubPage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using WeatherWizz.Common;
-using WeatherWizz.Data;
 using System;
 using System.Collections.Generic;
 using System.IO;
